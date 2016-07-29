5294d205970e9f313b6ff39a60669bf723d83dfd
dylee <neuralix@gmail.com>
2016-07-29 15:26:36 +0900

    possible for multi-gpu.

--- example/tiny.lua
+++ example/tiny.lua
@@ -1,14 +1,28 @@
+--[[
+Copyright (c) 2016-present, Facebook, Inc.
+All rights reserved.
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree. An additional grant
+of patent rights can be found in the PATENTS file in the same directory.
+]]--
+
+-- load torchnet:
 local tnt = require 'torchnet'
 
-flag_mnist = false 
+local flag_mnist = false
+local use_gpu = true
 
+-- function that sets of dataset iterator:
 local function getIterator(mode)
    return tnt.ParallelDatasetIterator{
       nthread = 1,
-      init = function() require 'torchnet' end,
+      init    = function() require 'torchnet' end,
       closure = function()
+
+         local data = nil
          if flag_mnist then
-            mnist = require 'mnist'
+            -- load MNIST dataset:
+            local mnist = require 'mnist'
             dataset = mnist[mode .. 'dataset']()
             dataset.data = dataset.data:reshape(dataset.data:size(1),
                dataset.data:size(2) * dataset.data:size(3)):double()
@@ -26,38 +40,40 @@ local function getIterator(mode)
                   labels = torch.Tensor(size)
                }
                dataset.data = torch.Tensor(size, 3*32*32)
-               dataset.labels = torch.Tensor(size)
+               dataset.label = torch.Tensor(size)
                for i = 0,4 do
                   subset = torch.load('cifar-10-batches-t7/data_batch_' .. (i+1) .. '.t7', 'ascii')
                   dataset.data[{ {i*10000+1, (i+1)*10000} }] = subset.data:t()
-                  dataset.labels[{ {i*10000+1, (i+1)*10000} }] = subset.labels
+                  dataset.label[{ {i*10000+1, (i+1)*10000} }] = subset.labels -- CAUTION: labels and label
                end
-               dataset.labels = dataset.labels + 1
+               dataset.label = dataset.label + 1
             else
                size = 2000
                dataset = {
                   data = torch.Tensor(size, 3*32*32),
-                  labels = torch.Tensor(size)
+                  label = torch.Tensor(size)
                }
                subset = torch.load('cifar-10-batches-t7/test_batch.t7', 'ascii')
                dataset.data = subset.data:t():double()
-               dataset.labels = subset.labels[1]:double()
-               dataset.labels = dataset.labels + 1
+               dataset.label = subset.labels[1]:double() -- CAUTION: labels and label
+               dataset.label = dataset.label + 1
             end
             -- resize dataset (if using small version)
             dataset.data = dataset.data[{ {1,size} }]
-            dataset.labels = dataset.labels[{ {1,size} }]
+            dataset.label = dataset.label[{ {1,size} }]
          end
+   
 
-         return tnt.BatchDataset{ 
-            batchsize = 1000,
-            dataset = tnt.ListDataset{
-               list = torch.range(1, dataset.data:size(1)):long(),            
+         -- return batches of data:
+         return tnt.BatchDataset{
+            batchsize = 128,
+            dataset = tnt.ListDataset{  -- replace this by your own dataset
+               list = torch.range(1, dataset.data:size(1)):long(),
                load = function(idx)
                   return {
                      input  = dataset.data[idx],
-                     target = torch.LongTensor{dataset.labels[idx] + 1},
-                  }
+                     target = torch.LongTensor{dataset.label[idx] + 1},
+                  }  -- sample contains input and target
                end,
             }
          }
@@ -65,57 +81,26 @@ local function getIterator(mode)
    }
 end
 
+require 'cunn'
+-- set up logistic regressor:
 local net = nil
 if flag_mnist then
-   net = nn.Sequential():add(nn.Linear(784, 10))
+   net = nn.Sequential():add(nn.Linear(784,10))
 else
-   channels = 3
-   net = nn.Sequential()
-   net:add(nn.Reshape(3*32*32))
-   net:add(nn.Linear(3*32*32, 10))
+   net = nn.Sequential():add(nn.Linear(3*32*32,10))
 end
+local criterion = nn.CrossEntropyCriterion()
 
---[[
-local orig = net:get(#net.modules)
-assert(torch.type(orig) == 'nn.Linear',
-   'expected last layer to be fully connected')
-
-local linear = nn.Linear(3*32*32, 10)
-linear.bias:zero()
-net:remove(#net.modules)
-net:add(linear:cuda())
-]]
-
--- Set the CUDNN flags
-require 'cunn'
-require 'cudnn'
-cudnn.fastest = true
-cudnn.benchmark = true
-
--- Wrap the model with DataParallelTable, if using more than one GPU
-local gpus = torch.range(1,1):totable()
-local fastest, benchmark = cudnn.fastest, cudnn.benchmark
-
-local dpt = nn.DataParallelTable(1, true, true)
-   :add(model, gpus)
-   :threads(function()
-      local cudnn = require 'cudnn'
-      cudnn.fastest, cudnn.benchmark = fastest, benchmark
-   end)
-dpt.gradInput = nil
-
+local dpt = nn.DataParallelTable(1)
+--dpt:add(net, {1,2,3,4})
+dpt:add(net, {1})
 net = dpt:cuda()
+crit = criterion:cuda()
 
-local crit = nn.CrossEntropyCriterion():cuda()
---local crit = nn.CrossEntropyCriterion()
-
-
-
-
-
+-- set up training engine:
 local engine = tnt.SGDEngine()
-local meter = tnt.AverageValueMeter()
-local clerr = tnt.ClassErrorMeter({topk={1}})
+local meter  = tnt.AverageValueMeter()
+local clerr  = tnt.ClassErrorMeter{topk = {1}}
 
 iter = 0
 old_iter = 0
@@ -126,14 +111,14 @@ engine.hooks.onStartEpoch = function(state)
    meter:reset()
    clerr:reset()
 end
-  
 engine.hooks.onForwardCriterion = function(state)
    iter = iter + 1
    meter:add(state.criterion.output)
    clerr:add(state.network.output, state.sample.target)
    if state.training then
       dummy = 1
-      --print(string.format('%d:%2.2f [iter:loss]', iter, meter:value()))
+      --print(string.format('avg. loss: %2.4f; avg. error: %2.4f',
+      --   meter:value(), clerr:value{k = 1}))
    end
 end
 
@@ -163,9 +148,6 @@ engine.hooks.onEndEpoch = function(state)
    end
 end
 
-require 'cunn'
-net = net:cuda()
-crit = crit:cuda()
 engine.hooks.onSample = function(state)
    state.sample.input = torch.CudaTensor():
       resize(state.sample.input:size()):
@@ -179,11 +161,31 @@ file = io.open('trend.log', 'a+')
 file:write(string.format('last\n'))
 file:close()
 
+-- set up GPU training:
+if usegpu then
+
+   -- copy model to GPU:
+   net       = net
+   criterion = crit
+
+   -- copy sample to GPU buffer:
+   local igpu, tgpu = torch.CudaTensor(), torch.CudaTensor()
+   engine.hooks.onSample = function(state)
+      igpu:resize(state.sample.input:size() ):copy(state.sample.input)
+      tgpu:resize(state.sample.target:size()):copy(state.sample.target)
+      state.sample.input  = igpu
+      state.sample.target = tgpu
+   end  -- alternatively, this logic can be implemented via a TransformDataset
+end
+
+-- train the model:
 engine:train{
-   network = net,
+   network   = net,
+   iterator  = getIterator('train'),
    criterion = crit,
-   iterator = getIterator('train'),
-   lr = 0.01,
-   maxepoch = 100000,
+   lr        = 0.2,
+   maxepoch  = 2,
 }
 
+print(string.format('test loss: %2.4f; test error: %2.4f',
+   meter:value(), clerr:value{k = 1}))
