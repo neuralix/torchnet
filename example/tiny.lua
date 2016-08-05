956d63844c396d08e4635a6d5748fdeb31a988a0
dylee <neuralix@gmail.com>
2016-08-05 17:33:36 +0900

    add model construnction routine for cifar-10

--- example/tiny.lua
+++ example/tiny.lua
@@ -9,7 +9,7 @@ of patent rights can be found in the PATENTS file in the same directory.
 -- load torchnet:
 local tnt = require 'torchnet'
 
-local flag_mnist = false
+local flag_mnist = false 
 local use_gpu = true
 
 -- function that sets of dataset iterator:
@@ -25,7 +25,7 @@ local function getIterator(mode)
             local mnist = require 'mnist'
             dataset = mnist[mode .. 'dataset']()
             dataset.data = dataset.data:reshape(dataset.data:size(1),
-               dataset.data:size(2) * dataset.data:size(3)):double()
+               1, dataset.data:size(2), dataset.data:size(3)):double()
          else
             if not paths.dirp('cifar-10-batches-t7') then
                print '==> downloading dataset'
@@ -60,6 +60,7 @@ local function getIterator(mode)
             end
             -- resize dataset (if using small version)
             dataset.data = dataset.data[{ {1,size} }]
+            dataset.data = dataset.data:reshape(size, 3, 32, 32)
             dataset.label = dataset.label[{ {1,size} }]
          end
    
@@ -82,13 +83,101 @@ local function getIterator(mode)
 end
 
 require 'cunn'
+
+
+
+
+
 -- set up logistic regressor:
 local net = nil
-if flag_mnist then
-   net = nn.Sequential():add(nn.Linear(784,10))
-else
-   net = nn.Sequential():add(nn.Linear(3*32*32,10))
+net = nn.Sequential()
+
+local function ConvBNReLU(nInputPlane, nOutputPlane)
+  net:add(nn.SpatialConvolution(nInputPlane, nOutputPlane, 3,3, 1,1, 1,1))
+  net:add(nn.SpatialBatchNormalization(nOutputPlane,1e-3))
+  net:add(nn.ReLU(true))
+  return net 
 end
+
+
+if flag_mnist then
+   net = nn.Sequential()
+   --net:add(nn.MulConstant(0.00390625))
+   net:add(nn.SpatialConvolution(1,20,5,5,1,1,0,0)) -- 1*28*28 -> 20*24*24
+   net:add(nn.SpatialMaxPooling(2,2,2,2)) -- 20*24*24 -> 20*12*12
+   net:add(nn.SpatialConvolution(20,50,5,5,1,1,0,0)) -- 20*12*12 -> 50*8*8
+   net:add(nn.SpatialMaxPooling(2,2,2,2)) --  50*8*8 -> 50*4*4
+   --net:add(nn.SpatialConvolution(1,20,5,5,1,1,0,0)) -- channels*28*28 -> 20*24*24
+   --net:add(nn.Linear(20*24*24,10))
+   --net = nn.Sequential()
+   --net:add(nn.MulConstant(0.00390625))
+   --net:add(nn.SpatialConvolution(channels,20,5,5,1,1,0)) -- channels*28*28 -> 20*24*24
+   --net:add(nn.SpatialMaxPooling(2, 2, 2, 2)) -- 20*24*24 -> 20*12*12
+   --net:add(nn.SpatialConvolution(20,50,5,5,1,1,0)) -- 20*12*12 -> 50*8*8
+   --net:add(nn.SpatialMaxPooling(2,2,2,2)) --  50*8*8 -> 50*4*4
+   --net:add(nn.View(-1):setNumInputDims(3))  -- 50*4*4 -> 800
+   --net:add(nn.Linear(800,500))  -- 800 -> 500
+   --net:add(nn.ReLU())
+   --net:add(nn.Linear(500, 10))  -- 500 -> nclasses
+   --net:add(nn.LogSoftMax())
+   net:add(nn.View(-1):setNumInputDims(3))  -- 50*4*4 -> 800
+   net:add(nn.Linear(50*4*4, 10))  -- 500 -> nclasses
+   net:add(nn.LogSoftMax())
+else
+   local MaxPooling = nn.SpatialMaxPooling
+   ConvBNReLU(3,64) -- 32*32 -> 30*30
+   --ConvBNReLU(64,64)
+   net:add(MaxPooling(2,2,2,2))
+   ConvBNReLU(64,128)
+   --ConvBNReLU(128,128)
+   net:add(MaxPooling(2,2,2,2))
+   ConvBNReLU(128,256)
+   --ConvBNReLU(256,256)
+   --ConvBNReLU(256,256)
+   net:add(MaxPooling(2,2,2,2))
+   ConvBNReLU(256,512)
+   --ConvBNReLU(512,512)
+   --ConvBNReLU(512,512)
+   net:add(MaxPooling(2,2,2,2))
+   --ConvBNReLU(512,512)
+   --ConvBNReLU(512,512)
+   --ConvBNReLU(512,512)
+   --net:add(MaxPooling(2,2,2,2))
+   --net:add(nn.View(512))
+   net:add(nn.View(-1):setNumInputDims(3))
+   --net:add(nn.Linear(512,512))
+   --net:add(nn.ReLU(true))
+   net:add(nn.Linear(512*(32/2/2/2/2)*(32/2/2/2/2),10)) 
+   -- initialization from MSR
+   local function MSRinit(net)
+     local function init(name)
+       for k,v in pairs(net:findModules(name)) do
+         local n = v.kW*v.kH*v.nOutputPlane
+         v.weight:normal(0,math.sqrt(2/n))
+         v.bias:zero()
+       end
+     end
+     -- have to do for both backends
+     --init'nn.SpatialConvolution'
+   end
+   MSRinit(net)
+end
+
+
+local function ConvInit(name)
+      for k,v in pairs(net:findModules(name)) do
+         local n = v.kW*v.kH*v.nOutputPlane
+         v.weight:normal(0,math.sqrt(2/n))
+         v.bias = nil
+         v.gradBias = nil
+      end
+end
+
+
+ConvInit('nn.SpatialConvolution')
+
+
+
 local criterion = nn.CrossEntropyCriterion()
 
 local dpt = nn.DataParallelTable(1)
@@ -184,7 +273,7 @@ engine:train{
    iterator  = getIterator('train'),
    criterion = crit,
    lr        = 0.2,
-   maxepoch  = 2,
+   maxepoch  = 10,
 }
 
 print(string.format('test loss: %2.4f; test error: %2.4f',
